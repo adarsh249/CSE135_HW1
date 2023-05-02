59b20a57f4d26d00c5be21c9af5ee4ba57f8c37d
Gary <atlae.ns@gmail.com>
2023-05-01 17:26:13 -0700

    we'll do it live

--- cgi-bin/Ruby/ruby-sessions-2.rb
+++ cgi-bin/Ruby/ruby-sessions-2.rb
@@ -3,32 +3,24 @@
 require 'cgi'
 require 'cgi/session'
 
-# Access Perl Session
+# Access Ruby Session
 session = CGI::Session.new(CGI.new, :cookie_only => false, :session_key => 'CGISESSID', :tmpdir => '/tmp')
 
 # Access Stored Data
 name = session['username']
 
+puts "Cache-Control: no-cache"
 puts "Content-Type: text/html\n\n"
 
-puts "<html>"
-puts "<head>"
-puts "<title>Perl Sessions</title>"
-puts "</head>"
-puts "<body>"
+puts "<html><head><title>Ruby Sessions</title></head>"
+puts "<body><h1 align=center>Ruby Sessions Page 2</h1><hr/>"
 
-puts "<h1>Perl Sessions Page 2</h1>"
-
-if name
-	puts("<p><b>Name:</b> #{name}")
-else
-	puts "<p><b>Name:</b> You do not have a name set</p>"
-end
+name ?	(puts "<p><b>Name:</b> #{name}") : (puts "<p><b>Name:</b> None</p>")
 
 puts "<br/><br/>"
-puts "<a href=\"/cgi-bin/perl-sessions-1.pl\">Session Page 1</a><br/>"
-puts "<a href=\"/perl-cgiform.html\">Perl CGI Form</a><br />"
-puts "<form style=\"margin-top:30px\" action=\"/cgi-bin/perl-destroy-session.pl\" method=\"get\">"
+puts "<a href=\"/cgi-bin/Ruby/ruby-sessions-1.rb\">Session Page 1</a><br/>"
+puts "<a href=\"/cgi-bin/Ruby/ruby-state-demo.rb\">Ruby CGI Form</a><br />"
+puts "<form style=\"margin-top:30px\" action=\"/cgi-bin/Ruby/ruby-destroy-session.rb\" method=\"get\">"
 puts "<button type=\"submit\">Destroy Session</button>"
 puts "</form>"
 
